f9cdfd75c1026f11fa0db6b267fea179180942b0
nanamiller <nana.miller313@gmail.com>
2025-03-20 23:10:15 -0400

    added doctring to vim

--- nana.py
+++ nana.py
@@ -1,8 +1,5 @@
 import numpy as np
-from scipy.signal import find_peaks
-
-def helloworld():
-    print("Hello world!")
+from scipy.signal import find_peaks)
 
 def get_started(num_of_peaks, xs, ys):
     """
@@ -27,7 +24,19 @@ def get_started(num_of_peaks, xs, ys):
     indxs, properties = find_peaks(ys)
     return indxs[np.argsort(-ys[indxs])[:num_of_peaks]]
 
+
 def check_inputs(xs):
+    """
+    
+    Checks whether the input array `xs` is sorted in ascending order.
+
+    Args:
+        xs (numpy.ndarray or list): The input array to check.
+
+    Returns:
+        bool: `True` if `xs` is sorted in ascending order, otherwise `False`.
+
+     """
     for i in range(len(xs)-1):
         if xs[i] > xs[i+1]:
             print("check_inputs(): input xs is badly ordered. Use reorder_inputs to reorder")
@@ -35,21 +44,107 @@ def check_inputs(xs):
     return True
 
 def reorder_inputs(xs,ys):
+
+    """
+    
+    Reorders the input arrays `xs` and `ys` in ascending order of `xs`.
+
+    Args:
+        xs (numpy.ndarray): The x-axis values 
+        ys (numpy.ndarray): The y-axis values
+        
+    Returns:
+        tuple of numpy arrays (sorted xs, sorted ys)
+
+    Bugs:
+        `xs` or `ys` must be NumPy array
+        `xs` and `ys`must be same length
+        
+    """
     i = np.argsort(xs)
     return xs[i], ys[i]
 
 def design_matrix(xlist):
+    """
+    
+    Constructs a design matrix for quadratic curve fitting.
+
+    Args:
+        xs (numpy.ndarray): The x-axis values 
+
+    Returns:
+        3 x 3 design matrix of numpy arrays
+
+    Bugs:
+        xlist must be an numpy array
+
+    Note:
+        Note the factor of 0.5 that Hogg likes and no one else
+        Also assumes xlist is ordered
+    
+    """
     return (np.vstack((xlist**0,xlist**1,0.5*xlist**2))).T
 
 def fit_parabola(xs, ys, index):
+    """
+
+    Fits a quadratic function to three consecutive data points. Solves for coefficients (b,m,q) in the quadratic
+    f(x) = b + m*x + (1/2) * q * x^2
+
+    Args:
+        xs (numpy.ndarray): The x-axis values 
+        ys (numpy.ndarray): The y-axis values
+        index (int): The index of peak
+        
+
+    Returns:
+        tuple: (b, m, q)
+
+    Bugs: 
+        index-1` or `index+2` can't be out of bounds
+        ``xs` or `ys` must be NumPy array
+        
+    
+    """
     return np.linalg.solve(design_matrix(xs[index-1:index+2]), ys[index-1:index+2])
 
 def refine_peak(xs,ys,index):
+    """
+
+    Refines the peak position using quadratic fit
+
+    Args:
+        xs (numpy.ndarray): The x-axis values 
+        ys (numpy.ndarray): The y-axis values
+        index (int): The index of peak
+    
+    Returns:
+        tuple: (x position of refined peak, y position of refined peak)
+
+    Bugs:
+        Must be synchronized with the design matrix
+        
+        
+    """
     b,m,q = fit_parabola(xs, ys, index)
     x_peak = -m / q
     return x_peak, 0.5 * q * (x_peak) ** 2 + m * (x_peak) + b
 
 def refine_peaks(xs, ys, indices):
+    """
+
+    Refines the peak position for a set of indices using quadratic fit
+
+    Args:
+        xs (numpy.ndarray): The x-axis values 
+        ys (numpy.ndarray): The y-axis values
+        indices (numpy array): indices of the peaks (this should be the output of get_started()
+
+    Returns:
+        tuple of numpy arrays (array of refined x positions, array of refined y positions
+    
+
+    """
     foo = lambda i: refine_peak(xs,ys,i)
     xs_refined, ys_refined = zip(*list(map,(foo,indices)))
     return np.array(xs_refined), np.array(ys_refined)
